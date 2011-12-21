285aa3f193c7cfc5e8681d5ff59ba3939a60a767
Kris Arnold <karnold@imamuseum.org>
2011-12-21 13:15:57 -0500

    added "asset-" to id

--- modules/tap/templates/tap-tourml-asset.tpl.php
+++ modules/tap/templates/tap-tourml-asset.tpl.php
@@ -1,3 +1,3 @@
-<tourml:Asset tourml:id="<?php print $asset['fid']; ?>">
+<tourml:Asset tourml:id="asset-<?php print $asset['fid']; ?>">
     <tourml:Source tourml:format="<?php print $asset['filemime']; ?>" tourml:lastModified="<?php print date('c', $asset['timestamp']); ?>" xml:lang="en" tourml:uri="<?php print $asset['uri']; ?>" />
 </tourml:Asset>
