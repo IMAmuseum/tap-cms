adeff2fc5c370359f7394f4d6ad5a1a9acba1a9e
Kris Arnold <karnold@imamuseum.org>
2012-01-05 09:32:15 -0500

    converted uri in assets to full path to file

--- modules/tap/templates/tap-tourml-asset.tpl.php
+++ modules/tap/templates/tap-tourml-asset.tpl.php
@@ -1,5 +1,5 @@
 <tourml:Asset tourml:id="asset-<?php print $asset['fid']; ?>">
-    <tourml:Source tourml:format="<?php print $asset['filemime']; ?>" tourml:lastModified="<?php print date('c', $asset['timestamp']); ?>" xml:lang="<?php print $language; ?>" tourml:uri="<?php print $asset['uri']; ?>">
+    <tourml:Source tourml:format="<?php print $asset['filemime']; ?>" tourml:lastModified="<?php print date('c', $asset['timestamp']); ?>" xml:lang="<?php print $language; ?>" tourml:uri="<?php print $path; ?>">
         <?php print $properties; ?>
     </tourml:Source>
 </tourml:Asset>
