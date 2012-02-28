2d8e6143e713feb992b1c3a68609c95f8a32ed82
Daniel Cervantes <dlcerva@gmail.com>
2012-02-28 03:15:35 -0500

    Added additional tap export functionality for ios bundling. Added TourMetadata element.

---
+++ modules/tap/templates/ios-plist-xml.tpl.php
@@ -0,0 +1,24 @@
+<?php print '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleDevelopmentRegion</key>
+	<string>English</string>
+	<key>CFBundleName</key>
+	<string><?php print $bundle_name; ?></string>
+	<key>CFBundleIconFile</key>
+	<string></string>
+	<key>CFBundleIdentifier</key>
+	<string><?php print $bundle_id; ?></string>
+	<key>CFBundleInfoDictionaryVersion</key>
+	<string>6.0</string>
+	<key>CFBundlePackageType</key>
+	<string>BNDL</string>
+	<key>CFBundleSignature</key>
+	<string>????</string>
+	<key>CFBundleVersion</key>
+	<string>1.0</string>
+	<key>NSPrincipalClass</key>
+	<string></string>
+</dict>
+</plist>
