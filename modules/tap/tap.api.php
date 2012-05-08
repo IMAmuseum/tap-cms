53cf275c39a10fad67d93ff7cf69d87030582484
Kris Arnold <karnold@imamuseum.org>
2012-05-08 11:43:08 -0400

    updated api docs

--- modules/tap/tap.api.php
+++ modules/tap/tap.api.php
@@ -15,29 +15,43 @@ function hook_tourml_asset(&$item) {
 }
 
 /**
- * Allow creation of extra properties to media assets
+ * Implements hook_tourml_asset()
  *
- * @param $item
- * The asset item to enact upon
- * 
- * @return Array
- * Return an array of key/value properties to be rendered with the asset
+ * @param $asset.
+ *
+ * @return $asset. An altered asset object
+ *
+ * Use this hook to render assets.  The geofield
+ * module stores its data in $asset['wkt'] so
+ * we can check against this value to determine
+ * that we have a geofield, and act upon it.
  */
-function hook_tourml_asset_properties($item) {
-    list($type, $ext) = explode('/', $item['filemime']);
-    switch($type) {
-        case 'image':
-            $path = drupal_realpath($item['uri']);
-            $attr = image_get_info($path);
-            return array(
-                array(
-                    'name'  => 'width',
-                    'value' => $attr['width'],
-                ),
-                array(
-                    'name'  => 'height',
-                    'value' => $attr['height'],
-                ),
-            );
-    }
+function tap_geo_tourml_asset($asset) {
+    // Handle geofield fields
+    if (isset($asset['wkt'])) {
+        geofield_load_geophp();
+
+        $geometry = geoPHP::load($asset['wkt'], 'wkt');
+        if ($geometry) {
+            $asset['value'] = $geometry->out('json');
+        }   
+    }   
+
+    return $asset;
 }
+
+/**
+ * Implements hook_tourml_asset_alter()
+ *
+ * Use this hook to define properties on an asset
+ */
+function tap_geo_tourml_asset_alter(&$tour, &$item) {
+    // Handle Geofields
+    if (isset($item['wkt'])) {
+        $item['properties'] = array(
+            'centroid' => $item['lat'] . ',' . $item['lon'],
+            'bbox' => $item['left'] . ',' . $item['right'] . ',' . $item['bottom'] . ',' . $item['top'],
+        );  
+    }   
+}
+
