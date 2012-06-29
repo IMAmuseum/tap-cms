08a4bbdb426245153ce9697fc01d55262275c773
Daniel Cervantes <dlcerva@gmail.com>
2012-06-29 14:55:10 -0400

    Updated asset ref to now check if the attached field is from the tour entity in order to use the 'AppResource' element instead.

--- modules/tap/templates/tap-tourml-asset-file.tpl.php
+++ modules/tap/templates/tap-tourml-asset-file.tpl.php
@@ -1,3 +1,7 @@
 <?php foreach ($items as $item): ?>
-<tourml:AssetRef tourml:id="asset-<?php print $item['id']; ?>" tourml:usage="<?php print $item['name']; ?>"></tourml:AssetRef>
+<?php if($item['field']['#bundle'] == 'tour'): ?>
+<tourml:AppResource tourml:id="asset-<?php print $item['id']; ?>" tourml:usage="<?php print $item['name']; ?>" />
+<?php else: ?>
+<tourml:AssetRef tourml:id="asset-<?php print $item['id']; ?>" tourml:usage="<?php print $item['name']; ?>" />
+<?php endif; ?>
 <?php endforeach; ?>
--- modules/tap/templates/tap-tourml-multipart-asset-file.tpl.php
+++ modules/tap/templates/tap-tourml-multipart-asset-file.tpl.php
@@ -1,3 +1,7 @@
-<?php foreach ($references as $id => $usage): ?>
-<tourml:AssetRef tourml:id="asset-<?php print $id; ?>" tourml:usage="<?php print $usage; ?>"></tourml:AssetRef>
+<?php foreach ($references as $id => $item): ?>
+<?php if($item['field']['#bundle'] == 'tour'): ?>
+<tourml:AppResource tourml:id="asset-<?php print $id; ?>" tourml:usage="<?php print $item['name']; ?>"/>
+<?php else: ?>
+<tourml:AssetRef tourml:id="asset-<?php print $id; ?>" tourml:usage="<?php print $item['name']; ?>"/>
+<?php endif; ?>
 <?php endforeach; ?>
