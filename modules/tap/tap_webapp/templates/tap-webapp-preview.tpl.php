adbb6e126006b82052f4aed6c56119bb80cb8be7
Kyle Jaebker <kjaebker@gmail.com>
2013-03-21 14:34:32 -0400

    add device wrapper around web app preview

--- modules/tap/tap_webapp/templates/tap-webapp-preview.tpl.php
+++ modules/tap/tap_webapp/templates/tap-webapp-preview.tpl.php
@@ -1,3 +1,3 @@
 <div id="tap-webapp">
-	<iframe src="<?php print $webapp_endpoint . '?tourml=' . $tourml_path; ?>" height="568" width="320"></iframe>
+	<iframe src="<?php print $webapp_endpoint . '?tourml=' . $tourml_path; ?>" height="568" width="320" frameBorder="0"></iframe>
 </div>
