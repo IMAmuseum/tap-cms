e2244946dd9c2a7416525367ddb7fe8643b35b55
Todd Kersey <tkersey@imamuseum.org>
2014-12-18 16:25:49 -0500

    Add DB calls for beacon data, form and table to display beacon data.

--- modules/tap/tap_beacons/templates/tap_beacons_template.tpl.php
+++ modules/tap/tap_beacons/templates/tap_beacons_template.tpl.php
@@ -1,3 +1,27 @@
 <div id="tap-beacons-admin">
-    yo
+    <div>
+        <form id="add-beacon-form">
+            <label>
+                UUID<br />
+                <input type="text" value="" name="uuid" id="beacon-uuid" class="form-text" size="60" />
+            </label>
+
+            <label>
+                Major Number<br />
+                <input type="text" value="" name="major_num" id="beacon-major" class="form-text" size="60" />
+            </label>
+
+            <label>
+                Minor Number<br />
+                <input type="text" value="" name="minor_num" id="beacon-minor" class="form-text" size="60" />
+            </label>
+
+            <br />
+
+            <input type="submit" value="Submit" class="form-submit" />
+        </form>
+    </div>
+
+    <?php print render($variables['beacons_data']); ?>
+
 </div>
