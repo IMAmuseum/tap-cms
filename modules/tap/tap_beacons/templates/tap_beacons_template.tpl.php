6ea06ebf50be7d432cf9cd53fccdfdbd9e92f1f8
Gray Bowman <gray.bowman@gmail.com>
2015-01-15 15:45:14 -0500

    moved beacon token to system var, removed singleton table, refactored api calls to use new token

--- modules/tap/tap_beacons/templates/tap_beacons_template.tpl.php
+++ modules/tap/tap_beacons/templates/tap_beacons_template.tpl.php
@@ -1,19 +1,5 @@
 <div class="tap-beacons-admin">
     <div>
-        <form action="<?php print url("admin/tap/beacons/tokens"); ?>" method="post" id="tap-beacons-add-token-form" class="tap-beacons-add-beacon-form">
-            <div class="general-errors"></div>
-            <input type="hidden" name="token_id" value="<?php (isset($variables['token_data'])) ? print '1' : print render($variables['token_data']['id']); ?>" />
-            <label>
-                Add a token for applications to send data to API end points.<br />
-                <input type="text" value="<?php print render($variables['token_data']['token']); ?>" name="token" id="beacon-token" class="form-text" size="60" data-required="true" />
-                <span class="tap-beacons-error-message"></span>
-            </label>
-
-            <input type="submit" value="Add Token" class="form-submit tap-beacons-add-submit" />
-        </form>
-    </div>
-
-    <div>
         <form action="<?php print url("admin/tap/beacons/add"); ?>" method="post" id="tap-beacons-add-beacon-form" class="tap-beacons-add-beacon-form">
             <div class="general-errors"></div>
             <p>All fields are required to add a new beacon.</p>
