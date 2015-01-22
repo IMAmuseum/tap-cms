674ec67c518cda315492f6b571e69e10b9e1657e
Todd Kersey <tkersey@imamuseum.org>
2015-01-22 11:20:28 -0500

    Add check for length on stop hidden input

--- modules/tap/tap_beacons/js/tap_beacons_charts.js
+++ modules/tap/tap_beacons/js/tap_beacons_charts.js
@@ -30,6 +30,10 @@ function drawPie(type, title) {
 
     var action = Drupal.settings.basePath + 'beacons/api/charts?type=' + type + '&' + window.location.search.substring(1);
 
+    if (jQuery('input[name="stop"]').length) {
+        action = action + '&stop=' + jQuery('input[name="stop"]').val();
+    }
+
     var jsonData = jQuery.ajax({
                 url: action,
                 dataType:"json",
