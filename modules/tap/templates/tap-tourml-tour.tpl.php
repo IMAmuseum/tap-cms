dc46c7865eb446830b9bd088fec3f1869ebb439c
Kyle Jaebker <kjaebker@gmail.com>
2015-10-14 12:13:12 -0400

    update psd to proper location. fixes #46

--- modules/tap/templates/tap-tourml-tour.tpl.php
+++ modules/tap/templates/tap-tourml-tour.tpl.php
@@ -1,10 +1,10 @@
 <?php print '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <tourml:Tour  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 	xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-	xmlns:tourml="http://tapintomuseums.org/TourML"
+	xmlns:tourml="http://tourml.org"
 	xmlns:xml="http://www.w3.org/XML/1998/namespace"
 	xmlns:gml="http://www.opengis.net/gml"
-	xsi:schemaLocation="http://tapintomuseums.org/TourML TourML.xsd"
+	xsi:schemaLocation="http://tourml.org/TourML.xsd"
 	tourml:id="tour-<?php print $tour->tour['real_id']; ?>"
 	tourml:lastModified="<?php print date('c', $tour->tour['last_modified']); ?>">
 <tourml:TourMetadata>
